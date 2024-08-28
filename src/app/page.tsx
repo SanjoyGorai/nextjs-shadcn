a37329ebb554430b8a756b7b837fd65b53f88f77
Mr. Sanjoy <--add>
2024-08-28 16:41:35 +0530

    InputOTP added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,6 +131,7 @@ const page = () => {
       <PaginationDemo/>
       <MenubarDemo/>
       <LabelDemo/>
+      <InputOTPDemo/>
     </div>
   );
 };
@@ -718,5 +719,29 @@ export function LabelDemo() {
         <Label htmlFor="terms">Accept terms and conditions</Label>
       </div>
     </div>
+  )
+}
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp"
+
+export function InputOTPDemo() {
+  return (
+    <InputOTP maxLength={6}>
+      <InputOTPGroup>
+        <InputOTPSlot index={0} />
+        <InputOTPSlot index={1} />
+        <InputOTPSlot index={2} />
+      </InputOTPGroup>
+      <InputOTPSeparator />
+      <InputOTPGroup>
+        <InputOTPSlot index={3} />
+        <InputOTPSlot index={4} />
+        <InputOTPSlot index={5} />
+      </InputOTPGroup>
+    </InputOTP>
   )
 }
