6becf9e76890d81bfad04ceeb774d904930caef8
Mr. Sanjoy <--add>
2024-08-28 12:12:08 +0530

    BadgeDemo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,6 +114,8 @@ const page = () => {
       <TabsDemo />
       <TooltipDemo />
       <ScrollAreaDemo/>
+      <AccordionDemo/>
+      <BadgeDemo/>
     </div>
   );
 };
@@ -187,4 +189,44 @@ export function ScrollAreaDemo() {
       </div>
     </ScrollArea>
   );
+}
+
+
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+export function AccordionDemo() {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It's animated by default, but you can disable it if you prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+}
+import { Badge } from "@/components/ui/badge"
+
+export function BadgeDemo() {
+  return <Badge>Badge</Badge>
 }
