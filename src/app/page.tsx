23720385c6de2fb49d88526f65ab3ca2ffc79cc6
Mr. Sanjoy <--add>
2024-08-28 17:09:46 +0530

    Carousel added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -135,11 +135,43 @@ const page = () => {
       <InputDemo/>
       <HoverCardDemo/>
       <DrawerDemo/>
+      <CarouselDemo/>
     </div>
   );
 };
 
 export default page;
+
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+ 
+export function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
 
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons"
 import { Bar, BarChart, ResponsiveContainer } from "recharts" 
