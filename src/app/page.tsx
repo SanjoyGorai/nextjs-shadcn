ff828dc45326ef74ee65db4a52695ce27652ce53
Mr. Sanjoy <--add>
2024-08-28 12:10:15 +0530

    shadcn, Separator scroll-area added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import React from "react";
 import Image from "next/image";
 import wild from "../../public/wild.webp";
 import {
@@ -43,6 +42,8 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
 const page = () => {
   return (
@@ -111,7 +112,8 @@ const page = () => {
       <AlertDialogDemo />
       <AvatarDemo />
       <TabsDemo />
-      <TooltipDemo/>
+      <TooltipDemo />
+      <ScrollAreaDemo/>
     </div>
   );
 };
@@ -162,5 +164,27 @@ export function TooltipDemo() {
         </TooltipContent>
       </Tooltip>
     </TooltipProvider>
+  );
+}
+
+const tags = Array.from({ length: 50 }).map(
+  (_, i, a) => `v1.2.0-beta.${a.length - i}`
+);
+
+export function ScrollAreaDemo() {
+  return (
+    <ScrollArea className="h-72 w-48 rounded-md border">
+      <div className="p-4">
+        <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
+        {tags.map((tag) => (
+          <>
+            <div key={tag} className="text-sm">
+              {tag}
+            </div>
+            <Separator className="my-2" />
+          </>
+        ))}
+      </div>
+    </ScrollArea>
   );
 }
